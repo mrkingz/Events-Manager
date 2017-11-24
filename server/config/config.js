099669908ea3fecf58fce9818664ba0e9211d321
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2017-11-24 07:47:56 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -35,7 +35,6 @@ module.exports = {
         logging: false,
     },  
     production: {
-        use_env_variable: 'URL',
-        dialect: 'postgres'
+        use_env_variable: 'DATABASE_URL'
     }
 }
